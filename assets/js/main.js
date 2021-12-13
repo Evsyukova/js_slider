64ef5c135b27036c898d5610a8bee51526483e03
Tanya Evsyukova <tanae3116@gmail.com>
2021-12-13 22:52:16 +0200

    created slides with buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,62 @@
 const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
+const pauseButton = document.querySelector('#pause');
+const previousButton = document.querySelector('#previous');
+const nextButton = document.querySelector('#next');
 
 const SLIDES_COUNT = slides.length;
-function nextSlide() {
+let currentSlide = 0;
+let timerID = null;
+let isPlaying = true;
+
+function gotoSlide(n) {
   slides[currentSlide].classList.toggle('active');
-  currentSlide = (currentSlide + 1) % SLIDES_COUNT;
+  currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
   slides[currentSlide].classList.toggle('active');
 }
 
-setInterval(nextSlide, 1000)
+function nextSlide() {
+  gotoSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  gotoSlide(currentSlide - 1);
+}
+
+function pauseSlideShow() {
+  if (isPlaying) {
+    clearInterval(timerID);
+    pauseButton.innerHTML = 'Play';
+    isPlaying = false;
+  }
+}
+
+function playSlideShow() {
+  timerID = setInterval(nextSlide, 1000);
+  pauseButton.innerHTML = 'Pause';
+  isPlaying = true;
+}
+
+function pausePlay() {
+  if (isPlaying) {
+    pauseSlideShow();
+  } else {
+    playSlideShow();
+  }
+}
+
+function prevHandler() {
+  pauseSlideShow();
+  prevSlide();
+}
+function nextHandler() {
+  pauseSlideShow();
+  nextSlide();
+}
+
+pauseButton.addEventListener('click', pausePlay);
+previousButton.addEventListener('click', prevHandler);
+nextButton.addEventListener('click', nextHandler);
+
+timerID = setInterval(nextSlide, 3000);
 
 
