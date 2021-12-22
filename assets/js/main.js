b2b73e33c138ec1762da6c80120a9c4d0583e4c1
Tanya Evsyukova <tanae3116@gmail.com>
2021-12-22 22:45:14 +0200

    keyboard control created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,38 @@
 const slides = document.querySelectorAll('.slide');
-const pauseButton = document.querySelector('#pause');
-const previousButton = document.querySelector('#previous');
-const nextButton = document.querySelector('#next');
+const indicatorsContainer = document.querySelector('#indicators-container');
+const indicators = document.querySelectorAll('.indicator');
+const pauseButton = document.querySelector('#pause-btn');
+const previousButton = document.querySelector('#prev-btn');
+const nextButton = document.querySelector('#next-btn');
 
 const SLIDES_COUNT = slides.length;
+const CODE_LEFT_ARROW = "ArrowLeft";
+const CODE_RIGHT_ARROW = "ArrowRight";
+const CODE_SPACE = "Space";
+
 let currentSlide = 0;
 let timerID = null;
 let isPlaying = true;
+let = interval = 1000;
 
-function gotoSlide(n) {
+function gotoNth(n) {
   slides[currentSlide].classList.toggle('active');
+  indicators[currentSlide].classList.toggle('active');
   currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
   slides[currentSlide].classList.toggle('active');
+  indicators[currentSlide].classList.toggle('active');
 }
 
-function nextSlide() {
-  gotoSlide(currentSlide + 1);
+function gotoPrev() {
+  gotoNth(currentSlide - 1);
 }
 
-function prevSlide() {
-  gotoSlide(currentSlide - 1);
+
+function gotoNext() {
+  gotoNth(currentSlide + 1);
 }
 
-function pauseSlideShow() {
+function pause() {
   if (isPlaying) {
     clearInterval(timerID);
     pauseButton.innerHTML = 'Play';
@@ -30,33 +40,44 @@ function pauseSlideShow() {
   }
 }
 
-function playSlideShow() {
-  timerID = setInterval(nextSlide, 1000);
+function play() {
+  timerID = setInterval(gotoNext, interval);
   pauseButton.innerHTML = 'Pause';
   isPlaying = true;
 }
 
-function pausePlay() {
-  if (isPlaying) {
-    pauseSlideShow();
-  } else {
-    playSlideShow();
+const pausePlay = () => isPlaying ? pause() : play();
+
+function prev() {
+  pause();
+  gotoPrev();
+}
+function next() {
+  pause();
+  gotoNext();
+}
+
+function indacate(e) {
+  const target = e.target;
+
+  if (target && target.classList.contains('indicator')) {
+    pause();
+    gotoNth(+target.dataset.slideTo);
   }
 }
 
-function prevHandler() {
-  pauseSlideShow();
-  prevSlide();
-}
-function nextHandler() {
-  pauseSlideShow();
-  nextSlide();
+function pressKey(e) {
+  if (e.code === CODE_LEFT_ARROW) prev();
+  if (e.code === CODE_RIGHT_ARROW) next();
+  if (e.code === CODE_SPACE) pausePlay();
 }
 
 pauseButton.addEventListener('click', pausePlay);
-previousButton.addEventListener('click', prevHandler);
-nextButton.addEventListener('click', nextHandler);
+previousButton.addEventListener('click', prev);
+nextButton.addEventListener('click', next);
+indicatorsContainer.addEventListener('click', indacate);
+document.addEventListener('keydown', pressKey)
 
-timerID = setInterval(nextSlide, 3000);
+timerID = setInterval(gotoNext, interval);
 
 
