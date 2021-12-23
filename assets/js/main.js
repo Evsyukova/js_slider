4b5393fc7544744237eba2de82ecb06a2bbe3de2
Tanya Evsyukova <tanae3116@gmail.com>
2021-12-23 21:43:30 +0200

    swipes created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,83 +1,102 @@
-const slides = document.querySelectorAll('.slide');
-const indicatorsContainer = document.querySelector('#indicators-container');
-const indicators = document.querySelectorAll('.indicator');
-const pauseButton = document.querySelector('#pause-btn');
-const previousButton = document.querySelector('#prev-btn');
-const nextButton = document.querySelector('#next-btn');
 
-const SLIDES_COUNT = slides.length;
-const CODE_LEFT_ARROW = "ArrowLeft";
-const CODE_RIGHT_ARROW = "ArrowRight";
-const CODE_SPACE = "Space";
+(function () {
 
-let currentSlide = 0;
-let timerID = null;
-let isPlaying = true;
-let = interval = 1000;
+  const container = document.querySelector('#carousel');
+  const slides = container.querySelectorAll('.slide');
+  const indicatorsContainer = container.querySelector('#indicators-container');
+  const indicators = indicatorsContainer.querySelectorAll('.indicator');
+  const pauseButton = container.querySelector('#pause-btn');
+  const previousButton = container.querySelector('#prev-btn');
+  const nextButton = container.querySelector('#next-btn');
 
-function gotoNth(n) {
-  slides[currentSlide].classList.toggle('active');
-  indicators[currentSlide].classList.toggle('active');
-  currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
-  slides[currentSlide].classList.toggle('active');
-  indicators[currentSlide].classList.toggle('active');
-}
+  const SLIDES_COUNT = slides.length;
+  const CODE_LEFT_ARROW = "ArrowLeft";
+  const CODE_RIGHT_ARROW = "ArrowRight";
+  const CODE_SPACE = "Space";
 
-function gotoPrev() {
-  gotoNth(currentSlide - 1);
-}
+  let currentSlide = 0;
+  let timerID = null;
+  let isPlaying = true;
+  let interval = 1000;
+  let swipeStartX = null;
+  let swipeEndX = null;
 
-
-function gotoNext() {
-  gotoNth(currentSlide + 1);
-}
-
-function pause() {
-  if (isPlaying) {
-    clearInterval(timerID);
-    pauseButton.innerHTML = 'Play';
-    isPlaying = false;
+  function gotoNth(n) {
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
+    currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
   }
-}
 
-function play() {
-  timerID = setInterval(gotoNext, interval);
-  pauseButton.innerHTML = 'Pause';
-  isPlaying = true;
-}
+  function gotoPrev() {
+    gotoNth(currentSlide - 1);
+  }
 
-const pausePlay = () => isPlaying ? pause() : play();
 
-function prev() {
-  pause();
-  gotoPrev();
-}
-function next() {
-  pause();
-  gotoNext();
-}
+  function gotoNext() {
+    gotoNth(currentSlide + 1);
+  }
 
-function indacate(e) {
-  const target = e.target;
+  function pause() {
+    if (isPlaying) {
+      clearInterval(timerID);
+      pauseButton.innerHTML = 'Play';
+      isPlaying = false;
+    }
+  }
 
-  if (target && target.classList.contains('indicator')) {
+  function play() {
+    timerID = setInterval(gotoNext, interval);
+    pauseButton.innerHTML = 'Pause';
+    isPlaying = true;
+  }
+
+  const pausePlay = () => isPlaying ? pause() : play();
+
+  function prev() {
     pause();
-    gotoNth(+target.dataset.slideTo);
+    gotoPrev();
   }
-}
+  function next() {
+    pause();
+    gotoNext();
+  }
 
-function pressKey(e) {
-  if (e.code === CODE_LEFT_ARROW) prev();
-  if (e.code === CODE_RIGHT_ARROW) next();
-  if (e.code === CODE_SPACE) pausePlay();
-}
+  function indacate(e) {
+    const target = e.target;
 
-pauseButton.addEventListener('click', pausePlay);
-previousButton.addEventListener('click', prev);
-nextButton.addEventListener('click', next);
-indicatorsContainer.addEventListener('click', indacate);
-document.addEventListener('keydown', pressKey)
+    if (target && target.classList.contains('indicator')) {
+      pause();
+      gotoNth(+target.dataset.slideTo);
+    }
+  }
 
-timerID = setInterval(gotoNext, interval);
+  function pressKey(e) {
+    if (e.code === CODE_LEFT_ARROW) prev();
+    if (e.code === CODE_RIGHT_ARROW) next();
+    if (e.code === CODE_SPACE) pausePlay();
+  }
 
+  function swipeStart(e) {
+    swipeStartX = e.changedTouches[0].pageX;
+  }
+
+  function swipeEnd(e) {
+    swipeEndX = e.changedTouches[0].pageX;
+    if (swipeStartX - swipeEndX < -100) prev();
+    if (swipeStartX - swipeEndX > 100) next();
+  }
+
+  pauseButton.addEventListener('click', pausePlay);
+  previousButton.addEventListener('click', prev);
+  nextButton.addEventListener('click', next);
+  indicatorsContainer.addEventListener('click', indacate);
+  container.addEventListener('touchstart', swipeStart);
+  container.addEventListener('touchend', swipeEnd);
+  document.addEventListener('keydown', pressKey)
+
+  timerID = setInterval(gotoNext, interval);
+
+}());
 
